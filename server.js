13d4b4132ad9c9758131ea8d398df84cb8b6e9bd
Imran Ahmed <100693340+imran-26@users.noreply.github.com>
2022-11-05 00:48:05 +0600

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const DB_URI = process.env.MONGO_URI;
 // Middleware
 app.use(
   cors({
-    origin: "https://frontendbrandr.vercel.app",
+    origin: "https://clientnextnodeproject.onrender.com",
     credentials: true,
   }),
 );
